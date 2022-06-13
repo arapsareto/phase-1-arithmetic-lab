796b93c7c6e3b1cd7e3fdb05f18606565273c812
martinsareto <martin.kiprotich@student.moringaschool.com>
2022-06-13 09:03:29 +0300

    <arithmetic lab>

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31;
 
-// Write your code here
+const num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random())+ 1;
+
+const num3 = 24;
+
+const num4 = 5;
+
+let mod = num3%num4;
+
+let max = Math.max(3,4,6,20)
